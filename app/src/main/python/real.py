352af3704d5bda35cbb471f042f8ab6c4febe263
IamGenius000 <47015944+IamGenius000@users.noreply.github.com>
2019-12-29 18:26:05 +0800

    Update real.py

--- app/src/main/python/real.py
+++ app/src/main/python/real.py
@@ -7,3 +7,7 @@ def mul(str1,str2):
     return str(sympify(str1)*sympify(str2))
 def div(str1,str2):
     return str(sympify(str1)/sympify(str2))
+def cal(str1):
+    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = symbols(
+    'a b c d e f g h i j k l m n o p q r s t u v w x y z')
+    return str(sympify(str1))
